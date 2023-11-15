5976d8be352228e2ce0eda1a129c9a9113882d51
Ayukamakin <anbrinchik@gmail.com>
2023-11-16 00:48:53 +0300

    1

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from tabulate import tabulate
+
 def secsum(a, b, c):
     s=str(a)+str(b)+str(c)
     meaning='0'
@@ -22,88 +25,108 @@ def goThrough(num1, num2):
         res=secsum(str(num1)[i], str(num2)[i],p)[0]+res
         p=secsum(str(num1)[i], str(num2)[i],p)[1]
     return res
-a = "1101"
-b = "1001"
-dop='0011'
-allBIN=[]
-allDESCOD=[]
-alldes={}
-correction={'1101':[], '0011':[]}
-binary_sum = lambda a, b: bin(int(a, 2) + int(b, 2))
-for a1 in '0','1':
-    for a2 in '0', '1':
-        for a3 in '0', '1':
-            for a4 in '0', '1':
-                allBIN.append(a1 + a2 + a3 + a4)
-                if int(a1+a2+a3+a4,2) <=9:
-                    alldes[int(a1+a2+a3+a4,2)]=binary_sum(a1+a2+a3+a4, dop)[2:]
-for i in alldes.keys():
-    if len(alldes[i])<4:
-        alldes[i]='0'*(4-len(alldes[i]))+alldes[i]
-print(alldes)
-print('сверху ожидаемое значение, под ним посчитанное, под подсчитанным коррекция, внизу итог')
-data=[]
-res={}
-for i in alldes.keys():
-    data.append([])
-    for j in alldes.keys():
-        sum=goThrough(alldes[i], alldes[j])
-        sumR=i+j
-        if sumR>=10:
-            sumR=sumR-10
-        data[int(i)].append(alldes[sumR]+'\n'+sum)
-        for cc in ('0011','0111','1101'):
-            sumK= goThrough(sum, cc)
-            if alldes[sumR]==sumK:
-                data[int(i)][int(j)] += '\n' + 'коррект '+cc+': '+sumK
-                if sum not in correction[cc]:
-                    correction[cc].append(sum)
+if __name__=="__main__":
+    timestats=[]
+    for i in range (0,100):
+        start_time=datetime.now()
+        a = "1101"
+        b = "1001"
+        dop='0011'
+        allBIN=[]
+        allDESCOD=[]
+        alldes={}
+        correction={'1101':[], '0011':[]}
+        binary_sum = lambda a, b: bin(int(a, 2) + int(b, 2))
+        for a1 in '0','1':
+            for a2 in '0', '1':
+                for a3 in '0', '1':
+                    for a4 in '0', '1':
+                        allBIN.append(a1 + a2 + a3 + a4)
+                        if int(a1+a2+a3+a4,2) <=9:
+                            alldes[int(a1+a2+a3+a4,2)]=binary_sum(a1+a2+a3+a4, dop)[2:]
+        for i in alldes.keys():
+            if len(alldes[i])<4:
+                alldes[i]='0'*(4-len(alldes[i]))+alldes[i]
+        print(alldes)
+        print('сверху ожидаемое значение, под ним посчитанное, под подсчитанным коррекция, внизу итог')
+        data=[]
+        res={}
+        for i in alldes.keys():
+            data.append([])
+            for j in alldes.keys():
+                sum=goThrough(alldes[i], alldes[j])
+                sumR=i+j
+                if sumR>=10:
+                    sumR=sumR-10
+                data[int(i)].append(alldes[sumR]+'\n'+sum)
+                for cc in ('0011','0111','1101'):
+                    sumK= goThrough(sum, cc)
+                    if alldes[sumR]==sumK:
+                        data[int(i)][int(j)] += '\n' + 'коррект '+cc+': '+sumK
+                        if sum not in correction[cc]:
+                            correction[cc].append(sum)
 
-print(tabulate(data, headers=alldes.keys(), tablefmt="fancy_grid", showindex="always")) #
-print(alldes)
-a=input()
-while a!='E':
-    word1=''
-    sum=[]
-    sum2=[]
-    word2=''
-    corr=[]
-    rsum=''
-    sumR=int(a.split()[0])+int(a.split()[1])
-    sumR=str(sumR)
-    for i in sumR:
-        rsum=rsum+alldes[int(i)]
-    for i in range(0, len(a.split()[0])):
-        word1+=alldes[int(a.split()[0][i])]
-        word2+=alldes[int(a.split()[1][i])]
-        sum.append(goThrough(alldes[int(a.split()[0][i])],alldes[int(a.split()[1][i])]))
-        if int(a.split()[0][i])+int(a.split()[1][i])<10:
-            sum2.append(goThrough(sum[-1], '1101'))
-            corr.append('1101')
-        else:
-            sum2.append(goThrough(sum[-1], '0011'))
-            corr.append('0011')
+        print(tabulate(data, headers=alldes.keys(), tablefmt="fancy_grid", showindex="always")) #
+        print(alldes)
+        #a=input()
+        a='E'
+        while a!='E':
+            word1=''
+            sum=[]
+            sum2=[]
+            word2=''
+            corr=[]
+            rsum=''
+            sumR=int(a.split()[0])+int(a.split()[1])
+            sumR=str(sumR)
+            for i in sumR:
+                rsum=rsum+alldes[int(i)]
+            for i in range(0, len(a.split()[0])):
+                word1+=alldes[int(a.split()[0][i])]
+                word2+=alldes[int(a.split()[1][i])]
+                sum.append(goThrough(alldes[int(a.split()[0][i])],alldes[int(a.split()[1][i])]))
+                if int(a.split()[0][i])+int(a.split()[1][i])<10:
+                    sum2.append(goThrough(sum[-1], '1101'))
+                    corr.append('1101')
+                else:
+                    sum2.append(goThrough(sum[-1], '0011'))
+                    corr.append('0011')
 
-    print(" "+word1)
-    print("+")
-    print(" "+word2)
-    print(' ------------')
-    print(" ", end='')
-    for i in reversed(sum):
-        print(i, end='')
-    print("\n+\n ", end='')
-    for i in reversed(corr):
-        print(i, end='')
-    print("\n ------------\n ", end='')
-    for i in reversed(sum2):
-        print(i, end='')
-    print("\n ------------\n ", end='')
-    print('верная сумма: '+rsum)
-    a=input()
-a=input()
-while a!='E':
-    for j in correction.keys():
-        for i in correction[j]:
-            if i==a:
-                print(j)
-    a = input()
+            print(" "+word1)
+            print("+")
+            print(" "+word2)
+            print(' ------------')
+            print(" ", end='')
+            for i in reversed(sum):
+                print(i, end='')
+            print("\n+\n ", end='')
+            for i in reversed(corr):
+                print(i, end='')
+            print("\n ------------\n ", end='')
+            for i in reversed(sum2):
+                print(i, end='')
+            print("\n ------------\n ", end='')
+            print('верная сумма: '+rsum)
+            a=input()
+        #a=input()
+        while a!='E':
+            for j in correction.keys():
+                for i in correction[j]:
+                    if i==a:
+                        print(j)
+            a = input()
+        end_time=datetime.now()
+        exec_time=end_time-start_time
+        print(f"Время исполнения: {exec_time}")
+        timestats.append(float(exec_time.total_seconds()))
+    timestats==[float(value) for value in sorted(timestats)]
+    print(sum(timestats))
+    n=len(timestats)
+    m=max(timestats)
+    mi=min(timestats)
+    if n%2==0:
+        median=(timestats[n//2-1]+timestats[n//2])/2
+    else:
+        median=timestats[n//2]
+    avg=sum(timestats)/n
+    print(f"Медианное время выполнения: {median}\nСреднее время выполнения :{avg}\nМаксимальное время выполнения: {m} \n Минимальное время выполнения {mi}")
